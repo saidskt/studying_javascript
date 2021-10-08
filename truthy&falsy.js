c35b6555dbc11b21adf77e83438e98304a4bfe24
Said Rabi Moreira <67801601+saidskt@users.noreply.github.com>
2021-10-08 12:34:41 -0300

    Add files via upload

---
+++ truthy&falsy.js
@@ -0,0 +1,16 @@
+const x = ``
+// FALSE
+
+const y = 0
+// FALSE
+
+const a = null
+const b = undefined
+// FALSE
+
+const lista = []
+const objeto ={}
+// TRUE
+
+// !! - Checa a variável se é true ou false da variável
+// ! - Inverte qualquer booleano, no caso se é true vira false 
