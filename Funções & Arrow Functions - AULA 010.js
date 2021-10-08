d1c23d2ff07dda4da40a8e5d3c8587aabe143fc1
Said Rabi Moreira <67801601+saidskt@users.noreply.github.com>
2021-10-08 12:35:12 -0300

    Add files via upload

---
+++ Funções & Arrow Functions - AULA 010.js
@@ -0,0 +1,15 @@
+function sum(a, b = 10){
+    return a + b
+}
+
+const sumArrow = (a = 2, b = 10) => {
+    return a + b 
+}
+
+
+// Mesma coisa que acima
+
+const sumArrow2 = ( a,b = 10) => a + b
+
+const sumValue = sumArrow2(2)
+console.log(sumValue)
